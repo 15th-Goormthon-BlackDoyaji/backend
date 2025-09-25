8fee84e236d312e19fb110fe6bd35afeef929f62
coli <kkwoo001021@naver.com>
2025-09-25 14:32:24 +0900

    feat: 레포지토리 계층 구현

---
+++ src/main/java/com/goormthon/repository/EducationInfoRepository.java
@@ -0,0 +1,8 @@
+package com.goormthon.repository;
+
+import com.goormthon.domain.EducationInfos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EducationInfoRepository extends JpaRepository<EducationInfos, Long> {
+
+}
---
+++ src/main/java/com/goormthon/repository/SubscriberRepository.java
@@ -0,0 +1,8 @@
+package com.goormthon.repository;
+
+import com.goormthon.domain.Subscriber;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscriberRepository extends JpaRepository<Subscriber, Long> {
+
+}
